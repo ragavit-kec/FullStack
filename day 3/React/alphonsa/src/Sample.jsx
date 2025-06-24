a142a5942d414ee6f70d0a4527b9c8d595e07d7a
ragavi <127740526+ragavit-kec@users.noreply.github.com>
2025-06-24 09:47:16 +0530

    Add files via upload

---
+++ day 3/React/alphonsa/src/Sample.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sample = (props) => {
+  return (
+    <div>
+        <h1>Sample {props.name}</h1>
+        <h1>Country:{props.country}</h1>
+    </div>
+
+  )
+}
+
---
+++ day 3/reduce.js
@@ -0,0 +1,3 @@
+const numbers=[10,20,30,40,50]
+const reducedValue = numbers.reduce((sum,n)=>sum+n,0)
+console.log(reducedValue); // Output: 150 
